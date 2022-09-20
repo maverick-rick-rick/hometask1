d980b199756337cc952023bb4f97c22148cfe483
maverick-rick-rick <113708668+maverick-rick-rick@users.noreply.github.com>
2022-09-20 16:20:27 +0300

    Version to make pull request

--- js/app.js
+++ js/app.js
@@ -1,132 +1,145 @@
-import {newNoteForm, appendVisibleTable, appendArchivedTable, newQuantityTable, createNewTableItem, moveNote, rerenderNoteTables, rerenderQuantityTable, fillFormWithExistingNote, editExistingNote} from "./modules/render/render.module.js";
+import {
+  newNoteForm,
+  appendVisibleTable,
+  appendArchivedTable,
+  newQuantityTable,
+  createNewTableItem,
+  moveNote,
+  rerenderNoteTables,
+  rerenderQuantityTable,
+  fillFormWithExistingNote,
+  editExistingNote,
+} from "./modules/render/render.module.js";
 
-//   
+document.addEventListener("DOMContentLoaded", () => {
+  //Some useful variables
 
-document.addEventListener('DOMContentLoaded', () => {
+  const popupWindow = document.querySelector(".popup");
+  const popupCloseBtn = document.getElementById("popup_close");
+  const newNoteBtn = document.getElementById("add_new_note");
+  const noteList = document.querySelector(".note_list > .container");
+  const noteCategories = document.querySelector(
+    ".note_categories > .container"
+  );
+  const popupContent = popupWindow.querySelector(".popup_content");
 
-	//Some useful variables
-	
-	const popupWindow = document.querySelector('.popup');
-	const popupCloseBtn = document.getElementById('popup_close');
-	const newNoteBtn = document.getElementById('add_new_note');
-	const noteList = document.querySelector('.note_list > .container');
-	const noteCategories = document.querySelector('.note_categories > .container');
-	const popupContent = popupWindow.querySelector('.popup_content');
-	
+  //Rendering table with existing notes
 
-	//Rendering table with existing notes
+  appendVisibleTable(noteList);
 
-	appendVisibleTable(noteList);
-		
-	//Rendering table with notes quantity
+  //Rendering table with notes quantity
 
-	noteCategories.append(newQuantityTable);
+  noteCategories.append(newQuantityTable);
 
-	// "Adding new note" functionality
+  // "Adding new note" functionality
 
-	newNoteBtn.addEventListener('click', function(){
-		popupContent.innerHTML = '';
-		newNoteForm.reset();
-		popupContent.append(newNoteForm);
-		popupWindow.classList.remove('hidden');
-		const noteListTableContent = noteList.querySelector('.table_content');
-		newNoteForm.addEventListener('submit', function evnt(e){
-        	e.preventDefault();
-			let result = createNewTableItem(newNoteForm);
-			noteListTableContent.append(result);
-			popupWindow.classList.add('hidden');
-			popupContent.innerHTML = '';
-			newNoteForm.removeEventListener('submit', evnt, false);
-    	}, false);
-	});
+  newNoteBtn.addEventListener("click", function () {
+    popupContent.innerHTML = "";
+    newNoteForm.reset();
+    popupContent.append(newNoteForm);
+    popupWindow.classList.remove("hidden");
+    const noteListTableContent = noteList.querySelector(".table_content");
+    newNoteForm.addEventListener(
+      "submit",
+      function evnt(e) {
+        e.preventDefault();
+        let result = createNewTableItem(newNoteForm);
+        noteListTableContent.append(result);
+        popupWindow.classList.add("hidden");
+        popupContent.innerHTML = "";
+        newNoteForm.removeEventListener("submit", evnt, false);
+      },
+      false
+    );
+  });
 
-	// Archieved notes
+  // Archieved notes
 
-	const archiveListButton = document.querySelector('.table_headline-buttons > .btn-archive');
-	archiveListButton.addEventListener('click', function(){
-		popupContent.innerHTML = '';
-		appendArchivedTable(popupContent);
-		popupWindow.classList.remove('hidden');
-	});
-	
-	
+  const archiveListButton = document.querySelector(
+    ".table_headline-buttons > .btn-archive"
+  );
+  archiveListButton.addEventListener("click", function () {
+    popupContent.innerHTML = "";
+    appendArchivedTable(popupContent);
+    popupWindow.classList.remove("hidden");
+  });
 
-	// Archive/Unarchive Note
+  // Archive/Unarchive Note
 
-	document.addEventListener('click', function(e){
-		let targetButtonSign = e.target.classList.contains('btn-archive');
-		if (!targetButtonSign || e.target.closest('.table_headline-buttons')) {
-			return false;
-		}
-		if (targetButtonSign) {
-			let currentNote = e.target.closest('.table_row');
-			let parentTable = e.target.closest('.table');
-			let typeOfParentTable = parentTable.dataset.type;
-			let currentNoteIndex = currentNote.dataset.index;
+  document.addEventListener("click", function (e) {
+    let targetButtonSign = e.target.classList.contains("btn-archive");
+    if (!targetButtonSign || e.target.closest(".table_headline-buttons")) {
+      return false;
+    }
+    if (targetButtonSign) {
+      let currentNote = e.target.closest(".table_row");
+      let parentTable = e.target.closest(".table");
+      let typeOfParentTable = parentTable.dataset.type;
+      let currentNoteIndex = currentNote.dataset.index;
 
-			moveNote(currentNoteIndex, typeOfParentTable);
+      moveNote(currentNoteIndex, typeOfParentTable);
 
-			rerenderNoteTables();
+      rerenderNoteTables();
 
-			const quantityTable = document.querySelector('.table_quantity');
-			rerenderQuantityTable(quantityTable);
-		}
-	});
+      const quantityTable = document.querySelector(".table_quantity");
+      rerenderQuantityTable(quantityTable);
+    }
+  });
 
-	// Edit note
+  // Edit note
 
-	document.addEventListener('click', function(e){
-		let targetButtonSign = e.target.classList.contains('btn-edit');
-		if (!targetButtonSign) {
-			return false;
-		}
-		if (targetButtonSign) {
-			let currentNote = e.target.closest('.table_row');
-			let parentTable = e.target.closest('.table');
-			let typeOfParentTable = parentTable.dataset.type;
-			let currentNoteIndex = currentNote.dataset.index;
+  document.addEventListener("click", function (e) {
+    let targetButtonSign = e.target.classList.contains("btn-edit");
+    if (!targetButtonSign) {
+      return false;
+    }
+    if (targetButtonSign) {
+      let currentNote = e.target.closest(".table_row");
+      let parentTable = e.target.closest(".table");
+      let typeOfParentTable = parentTable.dataset.type;
+      let currentNoteIndex = currentNote.dataset.index;
 
-			popupContent.innerHTML = '';
-			let editNoteForm = fillFormWithExistingNote(currentNoteIndex, typeOfParentTable);
-			popupContent.append(editNoteForm);
-			popupWindow.classList.remove('hidden');
+      popupContent.innerHTML = "";
+      let editNoteForm = fillFormWithExistingNote(
+        currentNoteIndex,
+        typeOfParentTable
+      );
+      popupContent.append(editNoteForm);
+      popupWindow.classList.remove("hidden");
 
-			editNoteForm.addEventListener('submit', function evnt(e){
-        	e.preventDefault();
+      editNoteForm.addEventListener(
+        "submit",
+        function evnt(e) {
+          e.preventDefault();
 
-			editExistingNote(editNoteForm, currentNoteIndex, typeOfParentTable);
+          editExistingNote(editNoteForm, currentNoteIndex, typeOfParentTable);
 
-			
-			popupWindow.classList.add('hidden');
-			popupContent.innerHTML = '';
-			rerenderNoteTables();
-			editNoteForm.removeEventListener('submit', evnt, false);
-    	}, false);
-			
-			
+          popupWindow.classList.add("hidden");
+          popupContent.innerHTML = "";
+          rerenderNoteTables();
+          editNoteForm.removeEventListener("submit", evnt, false);
+        },
+        false
+      );
+    }
+  });
 
-			
-		}
-	});
+  // Popup control
 
-	// Popup control
+  popupCloseBtn.addEventListener("click", function () {
+    this.closest(".popup").classList.add("hidden");
+  });
 
-	popupCloseBtn.addEventListener('click', function(){
-		this.closest('.popup').classList.add('hidden');
-	});
-
-	window.addEventListener('click', function(e){
-		if(e.target == newNoteBtn || e.target == archiveListButton || e.target.closest('.popup') || e.target.classList.contains('btn')) {
-			return false;
-		}
-		else {
-			popupWindow.classList.add('hidden');
-				
-		}
-	});
-	
+  window.addEventListener("click", function (e) {
+    if (
+      e.target == newNoteBtn ||
+      e.target == archiveListButton ||
+      e.target.closest(".popup") ||
+      e.target.classList.contains("btn")
+    ) {
+      return false;
+    } else {
+      popupWindow.classList.add("hidden");
+    }
+  });
 });
-
-
-
-
--- js/modules/data/content_data.module.js
+++ js/modules/data/content_data.module.js
@@ -1,92 +1,90 @@
 export let contentData = {
-    visibleNotes : [
-       {
-        name : 'Shopping list',
-        created : 'September 14, 2022',
-        category : 'Task',
-        content : 'Peanut, butter', 
-        deadlines : ''
-       },
-       {
-        name : 'Climb a tree',
-        created : 'September 22, 2022',
-        category : 'Random thought',
-        content : 'My neighbour have a bonsai tree...', 
-        deadlines : 'Start: 12.03.2022\nEnd: 12.10.2022'
-       },
-       {
-        name : 'Take a shower',
-        created : 'September 17, 2022',
-        category : 'Idea',
-        content : 'Or not...', 
-        deadlines : ''
-       },
-       {
-        name : 'Winter is coming',
-        created : 'September 14, 2022',
-        category : 'Quote',
-        content : 'Eddard Stark', 
-        deadlines : ''
-       },
-       {
-        name : 'To do',
-        created : 'February 14, 2022',
-        category : 'Random thought',
-        content : 'Ya bado bado', 
-        deadlines : ''
-       },
-       {
-        name : 'Buy a island',
-        created : 'April 22, 2022',
-        category : 'Idea',
-        content : 'On the Caribbean', 
-        deadlines : ''
-       },
-       {
-        name : 'It`s awesome',
-        created : 'August 12, 2022',
-        category : 'Quote',
-        content : 'Cartman', 
-        deadlines : ''
-       },
-       {
-        name : 'Something cool',
-        created : 'October 17, 2022',
-        category : 'Random thought',
-        content : '=__=', 
-        deadlines : 'Start: 17.07.2022\nEnd: 18.10.2022'
-       },
-
-    ],
-    archivedNotes : [
-        {
-        name : 'Shopping list',
-        created : 'September 14, 2022',
-        category : 'Task',
-        content : 'Peanut, butter', 
-        deadlines : ''
-       },
-       {
-        name : 'Climb a tree',
-        created : 'September 22, 2022',
-        category : 'Random thought',
-        content : 'My neighbour have a bonsai tree...', 
-        deadlines : 'Start: 22.05.2022\nEnd: 13.10.2022'
-       },
-       {
-        name : 'Take a shower',
-        created : 'September 17, 2022',
-        category : 'Idea',
-        content : 'Or not...', 
-        deadlines : ''
-       },
-       {
-        name : 'Winter is coming',
-        created : 'September 14, 2022',
-        category : 'Quote',
-        content : 'Eddard Stark', 
-        deadlines : ''
-       }
-    ]
-}
-
+  visibleNotes: [
+    {
+      name: "Shopping list",
+      created: "September 14, 2022",
+      category: "Task",
+      content: "Peanut, butter",
+      deadlines: "",
+    },
+    {
+      name: "Climb a tree",
+      created: "September 22, 2022",
+      category: "Random thought",
+      content: "My neighbour have a bonsai tree...",
+      deadlines: "Start: 12.03.2022\nEnd: 12.10.2022",
+    },
+    {
+      name: "Take a shower",
+      created: "September 17, 2022",
+      category: "Idea",
+      content: "Or not...",
+      deadlines: "",
+    },
+    {
+      name: "Winter is coming",
+      created: "September 14, 2022",
+      category: "Quote",
+      content: "Eddard Stark",
+      deadlines: "",
+    },
+    {
+      name: "To do",
+      created: "February 14, 2022",
+      category: "Random thought",
+      content: "Ya bado bado",
+      deadlines: "",
+    },
+    {
+      name: "Buy a island",
+      created: "April 22, 2022",
+      category: "Idea",
+      content: "On the Caribbean",
+      deadlines: "",
+    },
+    {
+      name: "It`s awesome",
+      created: "August 12, 2022",
+      category: "Quote",
+      content: "Cartman",
+      deadlines: "",
+    },
+    {
+      name: "Something cool",
+      created: "October 17, 2022",
+      category: "Random thought",
+      content: "=__=",
+      deadlines: "Start: 17.07.2022\nEnd: 18.10.2022",
+    },
+  ],
+  archivedNotes: [
+    {
+      name: "Shopping list",
+      created: "September 14, 2022",
+      category: "Task",
+      content: "Peanut, butter",
+      deadlines: "",
+    },
+    {
+      name: "Climb a tree",
+      created: "September 22, 2022",
+      category: "Random thought",
+      content: "My neighbour have a bonsai tree...",
+      deadlines: "Start: 22.05.2022\nEnd: 13.10.2022",
+    },
+    {
+      name: "Take a shower",
+      created: "September 17, 2022",
+      category: "Idea",
+      content: "Or not...",
+      deadlines: "",
+    },
+    {
+      name: "Winter is coming",
+      created: "September 14, 2022",
+      category: "Quote",
+      content: "Eddard Stark",
+      deadlines: "",
+    },
+  ],
+};
